c389361f76a735590b0b2b338e71032930047590
AlbertDev33 <alber725@gmail.com>
2021-02-05 17:48:58 -0300

    feat: add forecast configuration

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,25 @@
-console.log('test');
+import './util/module-alias';
+import { Server } from '@overnightjs/core';
+import bodyParser from 'body-parser';
+
+import { ForecasController } from './controllers/forecast';
+
+export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super();
+  }
+
+  public init(): void {
+    this.setupExpress();
+    this.setupControllers();
+  }
+
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
+
+  private setupControllers(): void {
+    const forecastController = new ForecasController();
+    this.addControllers([forecastController]);
+  }
+}
