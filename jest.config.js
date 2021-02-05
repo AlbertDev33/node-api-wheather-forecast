a9f6b7fb16b32839ba1812805bd85d84456c778a
AlbertDev33 <alber725@gmail.com>
2021-02-05 16:18:01 -0300

    chore: jest configuration

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+const { resolve } = require('path');
+const root = resolve(__dirname);
+
+module.exports = {
+  roots: root,
+  displayName: 'root-tests',
+  clearMocks: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  clearMocks: true,
+  coverageProvider: 'v8',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  moduleNameMapper: {
+    '@src/(.*)': '<rootDir>/src/$1',
+    '@test/(.*)': '<rootDir>/test/$1',
+  },
+};
