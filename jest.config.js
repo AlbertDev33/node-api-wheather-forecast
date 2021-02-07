c86afcc834e256a54bea8b99415e2a19f91503aa
AlbertDev33 <alber725@gmail.com>
2021-02-07 17:51:32 -0300

    chore: axios install and general config jest

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,21 @@
-// const { resolve } = require('path');
-import { resolve } from 'path';
+const { resolve } = require('path');
 
 const root = resolve(__dirname);
+
 
 module.exports = {
   roots: [root],
   displayName: 'root-tests',
   clearMocks: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
   transform: {
     '.+\\.ts$': 'ts-jest',
   },
   moduleNameMapper: {
-    '@src/(.*)': '<rootDir>/src/$1',
-    '@test/(.*)': '<rootDir>/test/$1',
-  },
+    "@src/(.*)": "<rootDir>/src/$1",
+    "@test/(.*)": "<rootDir>/test/$1",
+  }
 };
