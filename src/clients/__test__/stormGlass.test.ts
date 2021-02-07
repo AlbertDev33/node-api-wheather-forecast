27cb535abfe9b60d2cd57685e13db20df609c2f5
AlbertDev33 <alber725@gmail.com>
2021-02-07 17:53:22 -0300

    test: return the normalized forecast from StormGlass service

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,17 @@
+import { StormGlass } from '@src/clients/StormGlass';
+import axios from 'axios';
+
+jest.mock('axios');
+
+describe('StormGlass client', () => {
+  it('Sholuld return the normalized forecas from StormGlass service', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+
+    axios.get = jest.fn().mockResolvedValue({});
+
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual({});
+  });
+});
