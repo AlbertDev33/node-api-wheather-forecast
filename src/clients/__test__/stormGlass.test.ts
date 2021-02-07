fbdc3e00e03ea44528adc25a211de051133ad9e0
AlbertDev33 <alber725@gmail.com>
2021-02-07 20:50:17 -0300

    test: return the normalized forecas from StormGlass service

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,5 +1,8 @@
-import { StormGlass } from '@src/clients/StormGlass';
 import axios from 'axios';
+
+import { StormGlass } from '@src/clients/StormGlass';
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
 
 jest.mock('axios');
 
@@ -8,10 +11,12 @@ describe('StormGlass client', () => {
     const lat = -33.792726;
     const lng = 151.289824;
 
-    axios.get = jest.fn().mockResolvedValue({});
+    axios.get = jest
+      .fn()
+      .mockResolvedValue({ data: stormGlassWeather3HoursFixture });
 
     const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
-    expect(response).toEqual({});
+    expect(response).toEqual(stormGlassNormalized3HoursFixture);
   });
 });
