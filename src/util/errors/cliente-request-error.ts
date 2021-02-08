1bd9c1dbc57c3bbea0ac114ae470dfcbaa93f02e
AlbertDev33 <alber725@gmail.com>
2021-02-08 11:06:41 -0300

    feat: internal error handler

---
+++ src/util/errors/cliente-request-error.ts
@@ -0,0 +1,9 @@
+import { InternalError } from '@src/util/errors/interna-error';
+
+export class ClientRequestError extends InternalError {
+  constructor(message: string) {
+    const internalMessage =
+      'Unexpected error when trying to communicate to StormGlass';
+    super(`${internalMessage}: ${message}`);
+  }
+}
