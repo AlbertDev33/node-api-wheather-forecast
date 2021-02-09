5d211485a9b902f00281adc6edfaa03eda0a030f
AlbertDev33 <alber725@gmail.com>
2021-02-08 21:35:06 -0300

    feat: ensure that the return the forecast for a list of beaches

---
+++ src/services/forecast.ts
@@ -0,0 +1,45 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
+import { StormGlass, ForecastPoint } from '@src/clients/StormGlass';
+
+export enum BeachPosition {
+  S = 'S',
+  E = 'E',
+  W = 'W',
+  N = 'N',
+}
+
+export interface Beach {
+  name: string;
+  position: BeachPosition;
+  lat: number;
+  lng: number;
+  user: string;
+}
+
+export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
+
+export class Forecast {
+  constructor(protected stormGlass = new StormGlass()) {}
+
+  public async processForecastForBeaches(
+    beaches: Beach[],
+  ): Promise<BeachForecast[]> {
+    const pointWithCorrectSources: BeachForecast[] = [];
+    for (const beach of beaches) {
+      const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
+      const enrichedBeachData = points.map(response => ({
+        ...{
+          lat: beach.lat,
+          lng: beach.lng,
+          name: beach.name,
+          position: beach.position,
+          rating: 1,
+        },
+        ...response,
+      }));
+      pointWithCorrectSources.push(...enrichedBeachData);
+    }
+    return pointWithCorrectSources;
+  }
+}
