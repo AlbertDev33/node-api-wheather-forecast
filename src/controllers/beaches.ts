fb40597090cf5ff6fb443bf05e8bae8906a9d199
AlbertDev33 <alber725@gmail.com>
2021-02-10 15:39:44 -0300

    feat: create a beach on database

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,10 +1,23 @@
+import mongoose from 'mongoose';
+
 import { Controller, Post } from '@overnightjs/core';
+import { Beach } from '@src/models/beach';
 import { Request, Response } from 'express';
 
 @Controller('beaches')
 export class BeachesController {
   @Post('')
   public async create(request: Request, response: Response): Promise<void> {
-    response.status(201).send({ ...request.body, id: 'fake-id' });
+    try {
+      const beach = new Beach(request.body);
+      const result = await beach.save();
+      response.status(201).send(result);
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        response.status(422).send({ error: error.message });
+      } else {
+        response.status(500).send({ error: 'Internal Server Error' });
+      }
+    }
   }
 }
