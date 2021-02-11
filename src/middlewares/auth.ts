e8245da52e76cb4b9ae1f0c8e0ba55c7e03eaf38
AlbertDev33 <alber725@gmail.com>
2021-02-11 20:52:20 -0300

    feat: return a error if there is a problema on the token verification

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,12 +4,16 @@ import { AuthService } from '@src/services/auth';
 
 export function authMiddleware(
   request: Partial<Request>,
-  _: Partial<Response>,
+  response: Partial<Response>,
   next: NextFunction,
 ): void {
-  const token = request.headers?.['x-access-token'];
+  try {
+    const token = request.headers?.['x-access-token'];
 
-  const decoded = AuthService.decodeToken(token as string);
-  request.decoded = decoded;
-  next();
+    const decoded = AuthService.decodeToken(token as string);
+    request.decoded = decoded;
+    next();
+  } catch (err) {
+    response.status?.(401).send({ code: 401, error: err.message });
+  }
 }
