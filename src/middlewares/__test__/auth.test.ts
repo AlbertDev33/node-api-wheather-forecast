f1f386264e449f1526776734e627710ee9962069
AlbertDev33 <alber725@gmail.com>
2021-02-11 20:35:40 -0300

    test: validation user token

---
+++ src/middlewares/__test__/auth.test.ts
@@ -0,0 +1,19 @@
+import { AuthService } from '@src/services/auth';
+import { authMiddleware } from '@src/middlewares/auth';
+
+describe('AuthMiddleware', () => {
+  it('should verify a JWT token and call the next middleware', () => {
+    const jwtToken = AuthService.generateToken({ data: 'fake' });
+    const reqFake = {
+      headers: {
+        'x-access-token': jwtToken,
+      },
+    };
+
+    const resFake = {};
+    const nextFake = jest.fn();
+
+    authMiddleware(reqFake, resFake, nextFake);
+    expect(nextFake).toHaveBeenCalled();
+  });
+});
