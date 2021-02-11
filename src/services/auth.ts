9cd364aecbf48e93ef2583b608d6fa4732c3d225
AlbertDev33 <alber725@gmail.com>
2021-02-11 19:19:50 -0300

    refactor: config key jwt

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import config from 'config';
 
 export class AuthService {
   public static async hashPassword(
@@ -17,8 +18,8 @@ export class AuthService {
   }
 
   public static generateToken(payload: any): string {
-    return jwt.sign(payload, 'test', {
-      expiresIn: 10000,
+    return jwt.sign(payload, config.get('App.auth.key'), {
+      expiresIn: config.get('App.auth.tokenExpiresIn'),
     });
   }
 }
