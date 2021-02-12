f757f6075c8ad2f60768d7e4428f83e8bf5f77a5
AlbertDev33 <alber725@gmail.com>
2021-02-11 21:50:44 -0300

    refactor: find a beach for a especific user

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,20 +1,22 @@
-import { Controller, Get } from '@overnightjs/core';
+import { ClassMiddleware, Controller, Get } from '@overnightjs/core';
 import { Request, Response } from 'express';
 
 import { Forecast } from '@src/services/forecast';
 import { Beach } from '@src/models/beach';
+import { authMiddleware } from '@src/middlewares/auth';
 
 const forecast = new Forecast();
 
 @Controller('forecast')
+@ClassMiddleware(authMiddleware)
 export class ForecasController {
   @Get('')
   public async getForecastForLoggerdUser(
-    _: Request,
+    request: Request,
     response: Response,
   ): Promise<void> {
     try {
-      const beaches = await Beach.find({});
+      const beaches = await Beach.find({ user: request.decoded?.id });
       const forecastData = await forecast.processForecastForBeaches(beaches);
 
       response.status(200).send(forecastData);
