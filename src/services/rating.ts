9dc81964fb9ac888cc2a6eaf04ee41e4cf49dbb7
AlbertDev33 <alber725@gmail.com>
2021-02-15 16:50:37 -0300

    feat: return rating 5 for a beach with offshore winds

--- src/services/rating.ts
+++ src/services/rating.ts
@@ -1,6 +1,8 @@
-import { BeachPosition } from '@src/models/beach';
+import { Beach, BeachPosition } from '@src/models/beach';
 
 export class Rating {
+  constructor(private beach: Beach) {}
+
   public getRatingBasedOnWindAndWavePositions(
     wavePosition: BeachPosition,
     windPosition: BeachPosition,
@@ -8,6 +10,28 @@ export class Rating {
     if (wavePosition === windPosition) {
       return 1;
     }
+    if (this.isWindOffShore(wavePosition, windPosition)) {
+      return 5;
+    }
     return 3;
+  }
+
+  private isWindOffShore(
+    wavePosition: BeachPosition,
+    windPosition: BeachPosition,
+  ): boolean {
+    return (
+      (wavePosition === BeachPosition.N &&
+        this.beach.position === BeachPosition.N) ||
+      (wavePosition === BeachPosition.S &&
+        windPosition === BeachPosition.N &&
+        this.beach.position === BeachPosition.S) ||
+      (wavePosition === BeachPosition.E &&
+        windPosition === BeachPosition.W &&
+        this.beach.position === BeachPosition.E) ||
+      (wavePosition === BeachPosition.W &&
+        windPosition === BeachPosition.E &&
+        this.beach.position === BeachPosition.W)
+    );
   }
 }
