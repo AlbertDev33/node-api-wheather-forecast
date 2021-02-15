62d02e5f198230fc177856a4020440e9b342e976
AlbertDev33 <alber725@gmail.com>
2021-02-15 16:30:37 -0300

    feat: return 1 fora a beach with onshore winds

---
+++ src/services/rating.ts
@@ -0,0 +1,13 @@
+import { BeachPosition } from '@src/models/beach';
+
+export class Rating {
+  public getRatingBasedOnWindAndWavePositions(
+    wavePosition: BeachPosition,
+    windPosition: BeachPosition,
+  ): number {
+    if (wavePosition === windPosition) {
+      return 1;
+    }
+    return 0;
+  }
+}
