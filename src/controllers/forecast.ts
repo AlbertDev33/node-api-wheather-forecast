4468af94e7c3ecb41d5379a973e6199308d5ad46
AlbertDev33 <alber725@gmail.com>
2021-02-16 12:21:49 -0300

    feat: rate limit logical to StormGlass api request

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,17 +1,41 @@
-import { ClassMiddleware, Controller, Get } from '@overnightjs/core';
+import {
+  ClassMiddleware,
+  Controller,
+  Get,
+  Middleware,
+} from '@overnightjs/core';
 import { Request, Response } from 'express';
+import rateLimit from 'express-rate-limit';
 
 import { Forecast } from '@src/services/forecast';
 import { Beach } from '@src/models/beach';
 import { authMiddleware } from '@src/middlewares/auth';
+import ApiError from '@src/util/errors/api-error';
 import { BaseController } from '.';
 
 const forecast = new Forecast();
+
+const rateLimiter = rateLimit({
+  windowMs: 1 * 60 * 60 * 1000,
+  max: 3,
+  keyGenerator(request: Request): string {
+    return request.ip;
+  },
+  handler(_, response: Response): void {
+    response.status(429).send(
+      ApiError.format({
+        code: 429,
+        message: 'Too many requests to the /forecast endpoint',
+      }),
+    );
+  },
+});
 
 @Controller('forecast')
 @ClassMiddleware(authMiddleware)
 export class ForecasController extends BaseController {
   @Get('')
+  @Middleware(rateLimiter)
   public async getForecastForLoggerdUser(
     request: Request,
     response: Response,
