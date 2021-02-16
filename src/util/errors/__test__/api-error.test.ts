e090042b811c595d509d200dc3b75f33ab6d6309
AlbertDev33 <alber725@gmail.com>
2021-02-16 12:30:00 -0300

    test: format error with mandatory fields

--- src/util/errors/__test__/api-error.test.ts
+++ src/util/errors/__test__/api-error.test.ts
@@ -9,4 +9,36 @@ describe('ApiError', () => {
       code: 404,
     });
   });
+
+  it('should format error with mandatory fields and description', () => {
+    const error = ApiError.format({
+      code: 404,
+      message: 'User not found',
+      description: 'This error happens when there is no user created',
+    });
+
+    expect(error).toEqual({
+      message: 'User not found',
+      error: 'Not Found',
+      code: 404,
+      description: 'This error happens when there is no user created',
+    });
+  });
+
+  it('should format error with mandatory fields and description and documentation', () => {
+    const error = ApiError.format({
+      code: 404,
+      message: 'User not found',
+      description: 'This error happens when there is no user created',
+      documentation: 'https://mydocs.com/error-404',
+    });
+
+    expect(error).toEqual({
+      message: 'User not found',
+      error: 'Not Found',
+      code: 404,
+      description: 'This error happens when there is no user created',
+      documentation: 'https://mydocs.com/error-404',
+    });
+  });
 });
